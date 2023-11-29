8b1114eed32dd8bd2e1e9b712a5c33db7177185d
shubhamrustagi <rustagishubham2000@gmail.com>
2023-11-29 07:10:36 +0530

    fixed offline functionality

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,27 @@
-// service-worker.js
 
+const cacheName = 'cache-v1';
+
+const precacheResources = ['/', './index.html', './style.css', './index.js'];
+
+// When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('housie-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        './index.html',
-        './style.css',
-        './index.js',
-        '/images/image.png',
-        
-      ]);
-    })
-  );
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
 });
 
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
 self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request.url);
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
   );
 });
