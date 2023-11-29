238ed32867489ee1a44338f408be8341bf5a4bb1
shubhamrustagi <rustagishubham2000@gmail.com>
2023-11-29 07:19:32 +0530

    service worker bug fixes

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 
 const cacheName = 'cache-v1';
 
-const precacheResources = ['/', './index.html', './style.css', './index.js'];
+const precacheResources = ['/', '/index.html', '/style.css', '/index.js'];
 
-// When the service worker is installing, open the cache and add the precache resources to it
+
 self.addEventListener('install', (event) => {
   console.log('Service worker install event!');
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
@@ -13,7 +13,7 @@ self.addEventListener('activate', (event) => {
   console.log('Service worker activate event!');
 });
 
-// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+
 self.addEventListener('fetch', (event) => {
   console.log('Fetch intercepted for:', event.request.url);
   event.respondWith(
